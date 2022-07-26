155fe3f6be0c8998f4a6b036650daf10a99561ab
Hanrong YE <hyeae@cse.ust.hk>
2022-07-26 11:07:35 +0800

    clean code dir

--- code/run.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1,2 python -m torch.distributed.launch --nproc_per_node=3  main.py --config_exp './configs/pascal/pascal_vitLp16.yml' --run_mode train #--trained_model  pretrained_model_path
