89bc7fc8fcc40a3196a604f7e81be781f950cf7e
Hanrong YE <hyeae@cse.ust.hk>
2023-04-02 19:50:49 +0800

    rm useless

--- TaskPrompter/eval_edge.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-base=/data/hyeae/project/mtl/Multi-Task-Learning-PyTorch/eval_edge_files/
-subdir=$1
-
-mkdir "${base}${subdir}"
-cp -r ../$1/results/edge /data/hyeae/project/mtl/Multi-Task-Learning-PyTorch/eval_edge_files/$1/edge
