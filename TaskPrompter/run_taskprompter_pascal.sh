ac6c7dee2f3bcea068c7dfeede3ffa126d4d4d61
Hanrong YE <hyeae@cse.ust.hk>
2023-04-03 17:17:16 +0800

    update bs of pascal

--- TaskPrompter/run_taskprompter_pascal.sh
+++ TaskPrompter/run_taskprompter_pascal.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1,2 python -m torch.distributed.launch --nproc_per_node=3  --master_port=$((RANDOM%1000+12000))  main.py --config_exp './configs/pascal/pascal_vitLp16_taskprompter.yml' --run_mode train #--trained_model  pretrained_mode0l_path
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=3  --master_port=$((RANDOM%1000+12000))  main.py --config_exp './configs/pascal/pascal_vitLp16_taskprompter.yml' --run_mode train #--trained_model  pretrained_mode0l_path
