9206d232f44d0ddbedeaa2c1f3f46a872807dab1
Hanrong YE <hyeae@cse.ust.hk>
2023-05-07 19:00:55 +0800

    fix bug

--- TaskPrompter/models/taskprompter_wrapper.py
+++ TaskPrompter/models/taskprompter_wrapper.py
@@ -13,11 +13,17 @@ class TaskPrompterWrapper(nn.Module):
 
         self.backbone = backbone
         self.heads = heads 
-        self.target_size = p.dd_label_map_size
+
+        if 'dd_label_map_size' in p.keys():
+            self.target_size = p.dd_label_map_size
+        else:
+            self.target_size = None
 
     def forward(self, x):
         img_size = x.size()[-2:]
         out = {}
+
+        target_size = self.target_size if self.target_size is not None else img_size
 
         # TaskPrompter
         task_features, info = self.backbone(x) 
@@ -27,7 +33,7 @@ class TaskPrompterWrapper(nn.Module):
         for t in self.tasks:
             _task_fea = task_features[t]
             if t != '3ddet':
-                out[t] = F.interpolate(self.heads[t](_task_fea), self.target_size, mode=INTERPOLATE_MODE)
+                out[t] = F.interpolate(self.heads[t](_task_fea), target_size, mode=INTERPOLATE_MODE)
             else:
                 out[t] = self.heads[t](_task_fea)
             
